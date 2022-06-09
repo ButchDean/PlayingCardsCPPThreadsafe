f0a90b9befd00e438d49b94a11d95bf7b2fd491c
Dean Butcher <butchdeanca@gmail.com>
2022-06-09 00:52:29 -0400

    Fixed test to ensure each card is unique.

--- unittest/cardstest.cc
+++ unittest/cardstest.cc
@@ -3,6 +3,8 @@
 #include <cards.h>
 #include <functional.h>
 
+#include <vector>
+
 #define CARDDECKSIZE 52
 
 namespace cards {
@@ -15,7 +17,7 @@ TEST(PlayingCardsTest, InitDeckSize) {
     EXPECT_EQ(cardDeck.DeckSize(), CARDDECKSIZE);
 }
 
-TEST(PlayingCardsTest, Draw1CardDeckSize) {
+TEST(PlayingCardsTest, DrawSingleCardDeckSize) {
     CCardDeck cardDeck;
 
     cardDeck.Init();
@@ -23,6 +25,45 @@ TEST(PlayingCardsTest, Draw1CardDeckSize) {
     cardDeck.Draw();
     
     EXPECT_EQ(cardDeck.DeckSize(), CARDDECKSIZE - 1);
+}
+
+TEST(PlayingCardsTest, DrawToEmptyDeckWithZeroCardsRemaining) {
+    CCardDeck cardDeck;
+
+    cardDeck.Init();
+
+    CardRefs cr = cardDeck.Draw();
+
+    while(cr != EMPTY_DECK) {
+        cr = cardDeck.Draw();
+    }
+
+    EXPECT_EQ(cardDeck.DeckSize(), 0);
+}
+
+TEST(PlayingCardsTest, CheckEachCardUniqueUnshuffled) {
+    CCardDeck cardDeck;
+
+    cardDeck.Init();
+
+    CardRefs cr = SPADES_A;
+    std::vector<CardRefs> cardVec;
+    int same = 0;
+
+    while(cr != EMPTY_DECK) {
+        cr = cardDeck.Draw();
+        cardVec.push_back(cr);
+    }
+
+    for(int i = 0; i < cardVec.size() - 1; i++) {
+        for(int j = 0; j < cardVec.size() - 1; j++) {
+            if(cardVec[i] == cardVec[j]) {
+                same++;
+            }
+        }
+    }
+
+    EXPECT_EQ(same, CARDDECKSIZE);
 }
 
 }
