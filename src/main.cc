30eddd16a357a58f2febb24f14778c4b56210864
Dean Butcher <butchdeanca@gmail.com>
2022-07-29 15:02:24 -0400

    Been bugging me.

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,7 @@ extern unsigned int drawCount;
 extern unsigned int shuffleCount;
 
 int main() {
-	std::unique_ptr<cards::CCardDeck> cardDeck = std::make_unique<cards::CCardDeck>();
+	auto cardDeck = std::make_unique<cards::CCardDeck>();
 
 	// Initialize the deck
 	auto vf = std::async(std::launch::async, [&cardDeck]() {
@@ -33,8 +33,6 @@ int main() {
 			wf.get();
 		}
 	}
-
-	//threads.join();
 
 	std::printf("\nAttempted Draw Count: %d\nShuffle Count: %d\n", drawCount, shuffleCount);
 
