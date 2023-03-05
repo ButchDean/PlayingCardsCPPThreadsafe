d0a4cc4b3b28af91e790e5d51f09e85ed2df1583
Dean Butcher <butchdeanca@gmail.com>
2023-03-05 16:02:02 -0500

    Moved static functions to lambdas.

--- src/main.cc
+++ src/main.cc
@@ -10,39 +10,33 @@ static std::mutex mtx;
 static unsigned int drawCount = 0u; 
 static unsigned int shuffleCount = 0u;
 
-static void InitDeck(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	std::lock_guard<std::mutex> lg(mtx);
-	deckPtr->Init();
-}
-
-static void DrawCard(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	if(card != cards::CardRefs::EMPTY_DECK)
-	{
-		std::lock_guard<std::mutex> lg(mtx);
-		card = std::move(deckPtr->Draw());
-		drawCount++;
-
-		std::printf("Dealed card: %s\n", deckPtr->CardToStr(card).c_str());
-		std::printf("With value: %d\n", deckPtr->CardValue(card));
-	}
-}
-
-static void ShuffleDeck(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	std::lock_guard<std::mutex> lg(mtx);
-	deckPtr->Shuffle();
-	shuffleCount++;
-}
-
 int main() {
 	using funcWrap = std::function<void(std::unique_ptr<cards::CCardDeck>&&)>;
 
 	const std::vector<funcWrap> fw = {
-		std::bind(&InitDeck, std::placeholders::_1),
-		std::bind(&ShuffleDeck, std::placeholders::_1),
-		std::bind(&DrawCard, std::placeholders::_1)
+		// Initialize deck
+		std::bind([](std::unique_ptr<cards::CCardDeck>&& deckPtr) {
+			std::lock_guard<std::mutex> lg(mtx);
+			deckPtr->Init();
+		}, std::placeholders::_1),
+		// Shuffle deck
+		std::bind([](std::unique_ptr<cards::CCardDeck>&& deckPtr) {
+			std::lock_guard<std::mutex> lg(mtx);
+			deckPtr->Shuffle();
+			shuffleCount++;
+		}, std::placeholders::_1),
+		// Draw card
+		std::bind([](std::unique_ptr<cards::CCardDeck>&& deckPtr) {
+			if(card != cards::CardRefs::EMPTY_DECK)
+			{
+				std::lock_guard<std::mutex> lg(mtx);
+				card = std::move(deckPtr->Draw());
+				drawCount++;
+
+				std::printf("Dealt card: %s\n", deckPtr->CardToStr(card).c_str());
+				std::printf("With value: %d\n", deckPtr->CardValue(card));
+			}
+		}, std::placeholders::_1)
 	};
 
 	auto cardDeck = std::make_unique<cards::CCardDeck>();
