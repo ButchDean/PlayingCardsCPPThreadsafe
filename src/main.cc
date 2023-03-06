9297e40bc4a8f4d523d9d9c739b92e5616288f2d
Dean Butcher <butchdeanca@gmail.com>
2023-03-05 19:15:01 -0500

    Tidied up comments.

--- src/main.cc
+++ src/main.cc
@@ -14,18 +14,18 @@ int main() {
 	using funcWrap = std::function<void(std::unique_ptr<cards::CCardDeck>&)>;
 
 	const std::vector<funcWrap> fw = {
-		// Initialize deck [0]
+		// Initialize deck
 		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
 			std::lock_guard<std::mutex> lg(mtx);
 			deckPtr->Init();
 		}, std::placeholders::_1),
-		// Shuffle deck [1]
+		// Shuffle deck
 		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
 			std::lock_guard<std::mutex> lg(mtx);
 			deckPtr->Shuffle();
 			shuffleCount++;
 		}, std::placeholders::_1),
-		// Draw card [2]
+		// Draw card
 		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
 			if(card != cards::CardRefs::EMPTY_DECK)
 			{
