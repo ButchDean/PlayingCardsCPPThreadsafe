9048954e61575299b04f02d340de1e0e07e0bebd
Dean Butcher <butchdeanca@gmail.com>
2023-03-06 18:31:07 -0500

    Squashed commit of the following:
    commit 92e2c6b588c4a94ba7306bb3bc992c9467c5cf41
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Mon Mar 6 18:26:28 2023 -0500
    
        Remaining cards drawn by detached threads.
    
    commit 9297e40bc4a8f4d523d9d9c739b92e5616288f2d
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Mar 5 19:15:01 2023 -0500
    
        Tidied up comments.
    
    commit d05e001e10e7faf26a2583f5d1e1ccc8cc6324e3
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Mar 5 19:13:05 2023 -0500
    
        Draw threads are launched detached.
    
    commit 948fca3c25e81f675e06b8df9f78de0369a9ab23
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Mar 5 19:06:43 2023 -0500
    
        Multiple threads launched.
    
    commit d0a4cc4b3b28af91e790e5d51f09e85ed2df1583
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Mar 5 16:02:02 2023 -0500
    
        Moved static functions to lambdas.
    
    commit 9244b62cbdba15d3ec067b522a291ec600aa5cae
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sun Mar 5 00:14:14 2023 -0500
    
        Tidied things up with vector containing binds.
    
    commit 20f867d866499bcdbdc76f4439d2306258a3b5f4
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sat Mar 4 23:49:54 2023 -0500
    
        Proper use of std::bind.
    
    commit 5725abd63adeb763fc7df1354d9e62f1c9d6564c
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sat Mar 4 23:19:15 2023 -0500
    
        Tidied code up using std::function. Haven't switched rto std::bind yet.
    
    commit 6a8eecfccccbde0d3948443b4404aa99f965b856
    Author: Dean Butcher <butchdeanca@gmail.com>
    Date:   Sat Mar 4 17:30:25 2023 -0500
    
        Added std::bind.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdio>
-#include <future>
+#include <thread>
+#include <functional>
+#include <vector>
 #include <cards.h>
 
 static cards::CardRefs card = cards::CardRefs::CLUBS2;
@@ -8,64 +10,66 @@ static std::mutex mtx;
 static unsigned int drawCount = 0u; 
 static unsigned int shuffleCount = 0u;
 
-static void InitDeck(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	std::lock_guard<std::mutex> lg(mtx);
-	deckPtr->Init();
-	deckPtr->Shuffle();
-	shuffleCount++;
-}
-
-static void DrawCard(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	if(card != cards::CardRefs::EMPTY_DECK)
-	{
-		mtx.lock();
-		card = std::move(deckPtr->Draw());
-		drawCount++;
-		mtx.unlock();
-
-		std::printf("Dealed card: %s\n", deckPtr->CardToStr(card).c_str());
-		std::printf("With value: %d\n", deckPtr->CardValue(card));
-	}
-}
-
-static void ShuffleDeck(std::unique_ptr<cards::CCardDeck>&& deckPtr)
-{
-	std::lock_guard<std::mutex> lg(mtx);
-	deckPtr->Shuffle();
-	shuffleCount++;
-}
-
 int main() {
+	using funcWrap = std::function<void(std::unique_ptr<cards::CCardDeck>&)>;
+
+	const std::vector<funcWrap> fw = {
+		// Initialize deck
+		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
+			std::lock_guard<std::mutex> lg(mtx);
+			deckPtr->Init();
+		}, std::placeholders::_1),
+		// Shuffle deck
+		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
+			std::lock_guard<std::mutex> lg(mtx);
+			deckPtr->Shuffle();
+			shuffleCount++;
+		}, std::placeholders::_1),
+		// Draw card
+		std::bind([](std::unique_ptr<cards::CCardDeck>& deckPtr) {
+			if(card != cards::CardRefs::EMPTY_DECK)
+			{
+				std::lock_guard<std::mutex> lg(mtx);
+				card = std::move(deckPtr->Draw());
+				drawCount++;
+
+				std::printf("Dealt card: %s\n", deckPtr->CardToStr(card).c_str());
+				std::printf("With value: %d\n", deckPtr->CardValue(card));
+			}
+		}, std::placeholders::_1)
+	};
+
+	auto printStats = [=]() {
+		std::printf("\nAttempted Draw Count: %d\nShuffle Count: %d\n", drawCount, shuffleCount);
+	};
+
 	auto cardDeck = std::make_unique<cards::CCardDeck>();
 
-	// Initialize the deck
-	auto vf = std::async(std::launch::async, [&cardDeck]() {
-                            InitDeck(std::move(cardDeck));
-                            return true;
-                        });
-	vf.get();
+	// Events:
+	// 0 -> Initialize deck
+	// 1 -> Shuffle deck
+	// 2 -> Draw card
+	std::vector<int> events = {0, 1, 2, 2, 2, 2, 1, 2, 2, 0, 2, 2, 2, 1, 2, 2};
 
-	for(unsigned int idx = 0; idx < 60; idx++) {
-		// Shuffle on every tenth iteration
-		if(idx % 10 == 0) {
-			auto wf = std::async(std::launch::async, [&cardDeck]() {
-                                    ShuffleDeck(std::move(cardDeck));
-                                    return true;
-                                });
-			wf.get();
-		}
-		else {	// Attempt to draw card
-			auto wf = std::async(std::launch::async, [&cardDeck]() {
-                                    DrawCard(std::move(cardDeck));
-                                    return true;
-                                });
-			wf.get();
-		}
+	for(auto i : events) {
+		i = i % 3; // Ensure operations fall in range
+		std::thread e(fw[i], std::ref(cardDeck));
+
+		if(i == 3)
+			e.detach();
+		else
+			e.join();
 	}
 
-	std::printf("\nAttempted Draw Count: %d\nShuffle Count: %d\n", drawCount, shuffleCount);
+	printStats();
+
+	// Draw remaining cards (attrempting beyond empty)
+	for(int i = 0; i < 100; i++) {
+		std::thread e(fw[2], std::ref(cardDeck));
+		e.detach();
+	}
+
+	printStats();
 
 	return 0;
 }
